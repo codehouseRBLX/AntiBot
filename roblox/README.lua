5e770526ca74f9847e8e9393b4a54a3ac4aec239
therealkingnull <69044380+therealkingnull@users.noreply.github.com>
2021-03-27 11:24:59 -0400

    Update README.lua

--- roblox/README.lua
+++ roblox/README.lua
@@ -11,9 +11,9 @@
 AntiBot uses machine learning to determine the chance that a user that joins your game is a bot. AntiBot also includes a report feature, which helps the AI (Artificial Intelligence) preform better at finding bots.
 
 		SETUP:
-	- Ungroup the model (COMMAND/CONTROL + U) in "Workspace"
-	- Customize the settings in "AntiBotMain"
 	- ENABLE HTTP REQUESTS (under game settings). If this step is not done, the module won't be able to function
+	- Modify the settings in "AntiBotMain" to your liking
+	- Watch the scam bots go down!
 	
 Thanks for using AntiBot!
 ]]--
