bd8e4aa086681b736414dfec4599fc62a91ef893
therealkingnull <69044380+therealkingnull@users.noreply.github.com>
2021-03-26 20:23:11 -0400

    Create README.lua

---
+++ roblox/README.lua
@@ -0,0 +1,18 @@
+--[[
+                 _   _ ____        _   
+     /\         | | (_)  _ \      | |  
+    /  \   _ __ | |_ _| |_) | ___ | |_ 
+   / /\ \ | '_ \| __| |  _ < / _ \| __|
+  / ____ \| | | | |_| | |_) | (_) | |_ 
+ /_/    \_\_| |_|\__|_|____/ \___/ \__|
+                                       
+        The easiest way to keep scam bots out of your game - Created by lxgh1lxy
+	
+AntiBot uses machine learning to determine the chance that a user that joins your game is a bot. AntiBot also includes a report feature, which helps the AI (Artificial Intelligence) preform better at finding bots.
+
+		SETUP:
+	- Ungroup the model (COMMAND/CONTROL + U) in "Workspace"
+	- Customize the settings in "AntiBotMain"
+	
+Thanks for using AntiBot!
+]]--
