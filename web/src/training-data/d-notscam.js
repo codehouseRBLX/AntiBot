90b56cfbb9459e00ec866db4dfbbac6da4a1d9b6
therealkingnull <69044380+therealkingnull@users.noreply.github.com>
2021-03-26 20:20:04 -0400

    Create d-notscam.js

---
+++ web/src/training-data/d-notscam.js
@@ -0,0 +1,48 @@
+const notscam = [
+  {
+    input: "free robux please",
+    output: {notscam: 1}
+  },
+  {
+    input: "free robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i have free robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i have robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i have free robux please",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i have robux please",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i please have free robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i please have robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "gimme robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "give me robux",
+    output: {notscam: 1}
+  },
+  {
+    input: "can i please have robux please can i please have free robux",
+    output: {notscam: 1}
+  }
+]
+
+module.exports = notscam
